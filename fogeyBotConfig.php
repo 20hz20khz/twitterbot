007362fbfa378aa8e333e81c102805cb97b9fcb9
20hz20khz <20hz20khz@gmail.com>
2015-07-25 15:49:16 -0400

    1.0

---
+++ fogeyBotConfig.php
@@ -0,0 +1,36 @@
+<?php
+require_once('twitteroauth.php');
+
+define('CONSUMER_KEY', 'xxxxxxxxxxxxxxxxxxxx');
+define('CONSUMER_SECRET', 'xxxxxxxxxxxxxxxxxxxx');
+define('ACCESS_TOKEN', 'xxxxxxxxxxxxxxxxxxxx');
+define('ACCESS_TOKEN_SECRET', 'xxxxxxxxxxxxxxxxxxxx');
+
+$twitter = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
+$my_array = array("I recommend Glenn Miller","I recommend Benny Goodman","I recommend Mitch Miller","I recommend Lawrence Welk","I recommend Perry Como","I recommend Henry Mancini","I recommend Pat Boone");
+$output_count = 0;
+$twitter->host = "https://api.twitter.com/1.1/";
+$search = $twitter->get('search', array('q' => '%22recommend%20music%20for%20me%22', 'rpp' => 5));
+
+$twitter->host = "https://api.twitter.com/1.1/";
+foreach($search->results as $tweet) {
+	shuffle($my_array);
+	$status = $my_array[0].' @'.$tweet->from_user.' '.$tweet->text;
+	if(strlen($status) > 140) $status = substr($status, 0, 139);
+	$twitter->post('statuses/update', array('status' => $status));
+	$output_count++;
+}
+
+$twitter->host = "https://api.twitter.com/1.1/";
+$search = $twitter->get('search', array('q' => '%22recommend%20music%20to%20me%22', 'rpp' => 5));
+
+$twitter->host = "https://api.twitter.com/1.1/";
+foreach($search->results as $tweet) {
+	shuffle($my_array);
+	$status = $my_array[0].' @'.$tweet->from_user.' '.$tweet->text;
+	if(strlen($status) > 140) $status = substr($status, 0, 139);
+	$twitter->post('statuses/update', array('status' => $status));
+	$output_count++;
+}
+
+echo "Success! Check your twitter bot for ".$output_count." new tweets!";
