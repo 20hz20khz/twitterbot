7dd3b61fb756e85c4333d209b2d1fbf1e95e4dd2
20hz20khz <20hz20khz@gmail.com>
2015-07-27 21:09:06 -0400

    2.0

--- fogeyBotConfig.php
+++ fogeyBotConfig.php
@@ -1,38 +1,27 @@
 <?php
+$consumer_key = "XXXXXXXXXXXXXXX";// Redacted
+$consumer_secret = "XXXXXXXXXXXXXXX";// Redacted
+$access_key = "XXXXXXXXXXXXXXX";// Redacted
+$access_secret = "XXXXXXXXXXXXXXX";// Redacted
+
 require_once('twitteroauth.php');
 
-define('CONSUMER_KEY', 'xxxxxxxxxxxxxxxxxxxx'); // Redacted
-define('CONSUMER_SECRET', 'xxxxxxxxxxxxxxxxxxxx'); // Redacted
-define('ACCESS_TOKEN', 'xxxxxxxxxxxxxxxxxxxx'); // Redacted
-define('ACCESS_TOKEN_SECRET', 'xxxxxxxxxxxxxxxxxxxx'); // Redacted
-
-$twitter = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
+$twitter = new TwitterOAuth ($consumer_key ,$consumer_secret , $access_key , $access_secret );
 $my_array = array("I recommend Glenn Miller","I recommend Benny Goodman","I recommend Mitch Miller","I recommend Lawrence Welk","I recommend Perry Como","I recommend Henry Mancini","I recommend Pat Boone");
 $output_count = 0;
-$twitter->host = "https://api.twitter.com/1.1/";
-$search = $twitter->get('search/tweets', array('q' => '%22recommend%20music%20for%20me%22', 'rpp' => 5));
+$myUserInfo = $twitter->get('account/verify_credentials');
+$myLastTweet = $twitter->get('statuses/user_timeline', array('user_id' => $myUserInfo->id_str, 'count' => 1));
+$search = $twitter->get('search/tweets', array('q' => '%22recommend%20music%20for%20me%22', 'count' => 1, 'since_id' => $myLastTweet[0]->id_str));
 
-$twitter->host = "https://api.twitter.com/1.1/";
-foreach($search->results as $tweet) {
+foreach($search->statuses as $tweet) {
 	shuffle($my_array);
-	$status = $my_array[0].' @'.$tweet->from_user.' '.$tweet->text;
+	$status = $my_array[0].' @'.$tweet->user->screen_name;
 	if(strlen($status) > 140) $status = substr($status, 0, 139);
-	$twitter->post('statuses/update', array('status' => $status));
+	echo $status."\n";
+	$twitter->post('statuses/update', array('status' => $status,'in_reply_to_status_id' => $tweet->id_str));
 	$output_count++;
 }
 
-$twitter->host = "https://api.twitter.com/1.1/";
-$search = $twitter->get('search/tweets', array('q' => '%22recommend%20music%20to%20me%22', 'rpp' => 5));
+echo "Success! Check your twitter bot for ".$output_count." new tweets! My last tweet was ".$myLastTweet[0]->id_str;
 
-$twitter->host = "https://api.twitter.com/1.1/";
-foreach($search->results as $tweet) {
-	shuffle($my_array);
-	$status = $my_array[0].' @'.$tweet->from_user.' '.$tweet->text;
-	if(strlen($status) > 140) $status = substr($status, 0, 139);
-	$twitter->post('statuses/update', array('status' => $status));
-	$output_count++;
-}
-
-echo "Success! Check your twitter bot for ".$output_count." new tweets!";
-print_r( array_values( $search ));
 ?>
